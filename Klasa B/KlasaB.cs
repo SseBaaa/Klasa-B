594b8dc9ec93d43e3ed7838a4f16878fabb75108
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-10-16 22:06:05 +0200

    Add project files.

---
+++ Klasa B/KlasaB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa_B
+{
+    internal class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string stringUnos)
+        {
+            return stringUnos.Substring(1, stringUnos.Length - 2);
+
+        }
+    }
+}
---
+++ Klasa B/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+
+            string s = Console.ReadLine();
+
+            KlasaB x = new KlasaB();
+            if (s.Length > 2)
+            {
+                s = x.BezPrvogIZadnjeg(s);
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
